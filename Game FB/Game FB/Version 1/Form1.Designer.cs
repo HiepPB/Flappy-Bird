62b272f30e6114f330a8980521bfdb457e6c2ac9
HiepPB <51534464+HiepPB@users.noreply.github.com>
2019-06-10 00:01:19 +0700

    Add files via upload

---
+++ Game FB/Game FB/Version 1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Version_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ongtren1 = new System.Windows.Forms.PictureBox();
+            this.ongduoi1 = new System.Windows.Forms.PictureBox();
+            this.ongtren2 = new System.Windows.Forms.PictureBox();
+            this.ongduoi2 = new System.Windows.Forms.PictureBox();
+            this.conchim = new System.Windows.Forms.PictureBox();
+            this.nutplay = new System.Windows.Forms.PictureBox();
+            this.lblDiemSo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.conchim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nutplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ongtren1
+            // 
+            this.ongtren1.Image = ((System.Drawing.Image)(resources.GetObject("ongtren1.Image")));
+            this.ongtren1.Location = new System.Drawing.Point(60, -605);
+            this.ongtren1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.ongtren1.Name = "ongtren1";
+            this.ongtren1.Size = new System.Drawing.Size(206, 811);
+            this.ongtren1.TabIndex = 0;
+            this.ongtren1.TabStop = false;
+            // 
+            // ongduoi1
+            // 
+            this.ongduoi1.Image = ((System.Drawing.Image)(resources.GetObject("ongduoi1.Image")));
+            this.ongduoi1.Location = new System.Drawing.Point(54, 317);
+            this.ongduoi1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.ongduoi1.Name = "ongduoi1";
+            this.ongduoi1.Size = new System.Drawing.Size(212, 812);
+            this.ongduoi1.TabIndex = 1;
+            this.ongduoi1.TabStop = false;
+            // 
+            // ongtren2
+            // 
+            this.ongtren2.Image = ((System.Drawing.Image)(resources.GetObject("ongtren2.Image")));
+            this.ongtren2.Location = new System.Drawing.Point(362, -605);
+            this.ongtren2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.ongtren2.Name = "ongtren2";
+            this.ongtren2.Size = new System.Drawing.Size(206, 811);
+            this.ongtren2.TabIndex = 2;
+            this.ongtren2.TabStop = false;
+            // 
+            // ongduoi2
+            // 
+            this.ongduoi2.Image = ((System.Drawing.Image)(resources.GetObject("ongduoi2.Image")));
+            this.ongduoi2.Location = new System.Drawing.Point(362, 317);
+            this.ongduoi2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.ongduoi2.Name = "ongduoi2";
+            this.ongduoi2.Size = new System.Drawing.Size(212, 812);
+            this.ongduoi2.TabIndex = 3;
+            this.ongduoi2.TabStop = false;
+            // 
+            // conchim
+            // 
+            this.conchim.Image = ((System.Drawing.Image)(resources.GetObject("conchim.Image")));
+            this.conchim.Location = new System.Drawing.Point(9, 231);
+            this.conchim.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.conchim.Name = "conchim";
+            this.conchim.Size = new System.Drawing.Size(97, 81);
+            this.conchim.TabIndex = 4;
+            this.conchim.TabStop = false;
+            // 
+            // nutplay
+            // 
+            this.nutplay.Image = ((System.Drawing.Image)(resources.GetObject("nutplay.Image")));
+            this.nutplay.Location = new System.Drawing.Point(9, 10);
+            this.nutplay.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.nutplay.Name = "nutplay";
+            this.nutplay.Size = new System.Drawing.Size(37, 41);
+            this.nutplay.TabIndex = 5;
+            this.nutplay.TabStop = false;
+            // 
+            // lblDiemSo
+            // 
+            this.lblDiemSo.AutoSize = true;
+            this.lblDiemSo.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiemSo.Location = new System.Drawing.Point(2, 446);
+            this.lblDiemSo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDiemSo.Name = "lblDiemSo";
+            this.lblDiemSo.Size = new System.Drawing.Size(127, 45);
+            this.lblDiemSo.TabIndex = 6;
+            this.lblDiemSo.Text = "Score:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(677, 381);
+            this.Controls.Add(this.lblDiemSo);
+            this.Controls.Add(this.nutplay);
+            this.Controls.Add(this.conchim);
+            this.Controls.Add(this.ongduoi2);
+            this.Controls.Add(this.ongtren2);
+            this.Controls.Add(this.ongduoi1);
+            this.Controls.Add(this.ongtren1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongtren2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ongduoi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.conchim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nutplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ongtren1;
+        private System.Windows.Forms.PictureBox ongduoi1;
+        private System.Windows.Forms.PictureBox ongtren2;
+        private System.Windows.Forms.PictureBox ongduoi2;
+        private System.Windows.Forms.PictureBox conchim;
+        private System.Windows.Forms.PictureBox nutplay;
+        private System.Windows.Forms.Label lblDiemSo;
+    }
+}
+
---
+++ Game FB/Game FB/Version 1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int DoChenhLechGiua2OngCong = 250;
+
+        int x_cap1, x_cap2;
+
+        int y_ongtren1;
+        int y_ongduoi1;
+
+        int y_ongtren2;
+        int y_ongduoi2;
+
+        int x_conchim, y_conchim;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Left = 0;
+            this.Top = 0;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            // MessageBox.Show("Width = " + this.Width + " & Height = " + this.Height);
+
+            x_cap1 = this.Width + 300;
+
+           
+            y_ongtren1 = -700;
+            ongtren1.Size = new Size(270, 1000);
+            ongtren1.Location = new Point(x_cap1, y_ongtren1);
+            y_ongduoi1 = (1000 + y_ongtren1) + DoChenhLechGiua2OngCong;
+            ongduoi1.Size = new Size(270, 1000);
+            ongduoi1.Location = new Point(x_cap1, y_ongduoi1);
+            x_cap2 = x_cap1 + ongtren1.Width + 400;        
+            y_ongtren2 = -700;
+            ongtren2.Size = new Size(270, 1000);
+            ongtren2.Location = new Point(x_cap2, y_ongtren2);
+
+            
+            y_ongduoi2 = (1000 + y_ongtren2) + DoChenhLechGiua2OngCong;
+            ongduoi2.Size = new Size(270, 1000);
+            ongduoi2.Location = new Point(x_cap2, y_ongduoi2);
+
+            conchim.Size = new Size(127, 100);
+            x_conchim = conchim.Location.X;
+            y_conchim = conchim.Location.Y;
+
+            nutplay.Size = new System.Drawing.Size(50, 50);
+
+            lblDiemSo.Location = new Point(0, this.Height - 50);          
+        }
+        }
+}
